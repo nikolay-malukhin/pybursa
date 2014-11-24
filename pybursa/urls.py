8f6dba692c6e6e851aea2d656ea8486880c53e43
Oleksandr Berezovskyi <berezovskyi.oleksandr@gmail.com>
2014-11-25 01:59:50 +0200

    HW #7 solved

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,12 +1,19 @@
 from django.views.generic import TemplateView
 
-
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 
 urlpatterns = patterns('',
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
-    url(r'^admin/', include(admin.site.urls)),
+                       # url(r'^blog/', include('blog.urls')),
+                       url(r'^$',
+                           TemplateView.as_view(template_name='index.html'),
+                           name='index'),
+                       url(r'^products/$',
+                           TemplateView.as_view(template_name='products.html'),
+                           name='products'),
+                       url(r'^product/$',
+                           TemplateView.as_view(template_name='product.html'),
+                           name='product'),
+                       url(r'^admin/', include(admin.site.urls)),
 )
